2d5238c62cc0ef1e9927baedf2c0b885f36f05b4
ab-h-i-n <abhinshelly03@gmail.com>
2025-11-01 16:47:51 +0530

    done

---
+++ public/scripts/getMenu.js
@@ -0,0 +1,32 @@
+console.log("Fetching menu data...");
+
+const getMenu = async () => {
+  try {
+
+    const url = new URL(window.location.href);
+    const partner = url.searchParams.get("partner");
+
+    //get the menu/partner folder items
+    const response = await fetch(`/menus/${partner}/data.json`);
+    const dataJson = await response.json();
+
+    const numberOfPages = dataJson.num_of_pages; 
+
+    console.log(`Menu for partner ${partner} has ${numberOfPages} pages.`);
+
+    const menuContainer = document.getElementById("menu-container");    
+    for(let i=1; i<=numberOfPages; i++) {
+        const img = document.createElement("img");
+        img.src = `/menus/${partner}/page_${i}.webp`;
+        img.alt = `Menu Page ${i}`;
+        img.className = "w-full mb-4 rounded-lg shadow-md";
+        menuContainer.appendChild(img);
+    }
+
+  } catch (error) {
+    console.error("Error fetching menu data:", error);
+  }
+};
+
+
+getMenu();
---
+++ public/scripts/getPartner.js
@@ -0,0 +1,27 @@
+console.log("Fetching partner data...");
+
+const getPartners = async () => {
+  try {
+    const response = await fetch("/data/partners.json");
+    const partners = await response.json();
+
+
+    console.log("Partners loaded:", partners);
+
+
+    const partnerList = document.getElementById("partner-list");
+
+    partners.forEach(partner => {
+      const listItem = document.createElement("a");
+      listItem.innerText = partner;
+      listItem.href = `/menu?partner=${partner}`;
+      partnerList.appendChild(listItem);
+    });
+
+  } catch (error) {
+    console.error("Error fetching partner data:", error);
+  }
+};
+
+
+getPartners();
